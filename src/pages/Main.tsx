da47756fa0a83e00f5c5077c15edea3813334b43
jazzo5947 <jazzo5947@gmail.com>
2024-01-04 15:13:24 +0900

    fix: esLint 설정 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,8 +2,6 @@ import * as React from "react"
 
 import { Button } from "@mui/base"
 
-import axios from "axios"
-
 // todo: 로그인하기, 회원가입, 둘러보기?
 
 const signUpURL = "/sign-up"
@@ -12,7 +10,7 @@ function MainPage() {
   return (
     <>
       <h1 className="">Welcome to CoteChat!</h1>
-      <Button />
+      <Button href="/login">Login</Button>
     </>
   )
 }
