2499d3c85987b0d60c08c2a51abd5cd1fe4b2545
jazzo5947 <jazzo5947@gmail.com>
2024-01-07 16:25:20 +0900

    chore: tailwindCSS prefix 설정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,39 @@
 import * as React from "react"
 
+import { Link } from "react-router-dom"
+
 function LoginPage() {
-  React.useEffect(() => {}, [])
+  const [clientId, setClientId] = React.useState("")
+
+  const githubURL = `https://github.com/login/oauth/authorize`
+  const config = `?scope=user:email,public_repo&client_id=`
+  const onChangeId = (e) => {
+    setClientId(e.target.value)
+  }
 
   return (
-    <>
-      <h1>로그인 페이지입니다</h1>
-      <form>
-        <label className="tw-flex">
-          <p>아이디</p>
-          <input className="tw-border-[#c2c2c2]" />
+    <div className="tw-text-center">
+      <h1 className="tw-mb-20 tw-font-bold">~ Login Page ~</h1>
+
+      <div className="tw-flex tw-flex-col tw-items-center">
+        <label htmlFor="clientID" className="">
+          아이디
         </label>
-        <label className="tw-flex">
-          <p>비밀번호</p>
-          <input />
-        </label>
-      </form>
-    </>
+        <input
+          id="clientId"
+          value={clientId}
+          onChange={onChangeId}
+          className="tw-mb-20 tw-w-200 tw-border tw-border-[#c2c2c2]"
+        />
+        <Link
+          to={githubURL + config + clientId}
+          type="button"
+          className="tw-w-200 tw-cursor-pointer tw-rounded tw-bg-gray-200 tw-px-20 tw-py-10 tw-text-center"
+        >
+          로그인
+        </Link>
+      </div>
+    </div>
   )
 }
 
