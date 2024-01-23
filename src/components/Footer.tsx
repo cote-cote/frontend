2eda4476ed8e2ce6e5f198fa49e4f76d9898e919
jazzo947 <jazzo5947@gmail.com>
2024-01-23 23:40:25 +0900

    add Elliot's email

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ function Footer() {
   return (
     <footer className="fixed bottom-0 flex h-100 w-full flex-col items-center justify-center bg-black text-center">
       <p className="text-white">© 2024 CoteCote. All rights reserved.</p>
-      <p className="text-white">Contact: jazzo5947@gmail.com / </p>
+      <p className="text-white">
+        Contact: jazzo5947@gmail.com / yimsb02@naver.com
+      </p>
     </footer>
   )
 }
