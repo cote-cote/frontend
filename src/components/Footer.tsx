ba177b1b6885d2ecd6be369d541f8de6357d6124
jazzo5947 <jazzo5947@gmail.com>
2024-01-31 21:05:09 +0900

    fix: fix common layout prefix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
-import * as React from "react"
+import * as React from "react";
 
 function Footer() {
   return (
-    <footer className="fixed bottom-0 flex h-100 w-full flex-col items-center justify-center bg-black text-center">
-      <p className="text-white">© 2024 CoteCote. All rights reserved.</p>
-      <p className="text-white">
+    <footer className="tw-flex tw-h-100 tw-w-full tw-flex-col tw-items-center tw-justify-center tw-bg-black tw-text-center">
+      <p className="tw-text-white">© 2024 CoteCote. All rights reserved.</p>
+      <p className="tw-text-white">
         Contact: jazzo5947@gmail.com / yimsb02@naver.com
       </p>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
