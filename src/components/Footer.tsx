343a8b36e64130a70e2f4b8be7713f51afc0bbef
jazzo5947 <jazzo5947@gmail.com>
2024-02-04 15:37:41 +0900

    fix: 헤더와 푸터 css 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 
 function Footer() {
   return (
-    <footer className="tw-flex tw-h-100 tw-w-full tw-flex-col tw-items-center tw-justify-center tw-bg-black tw-text-center">
-      <p className="tw-text-white">© 2024 CoteCote. All rights reserved.</p>
-      <p className="tw-text-white">
+    <footer className="tw-flex tw-w-full tw-flex-col tw-items-center tw-justify-center tw-bg-gray-200 tw-py-20 tw-text-center">
+      <p className="tw-text-12 tw-text-black">
+        © 2024 CoteCote. All rights reserved.
+      </p>
+      <p className="tw-text-12 tw-text-black">
         Contact: jazzo5947@gmail.com / yimsb02@naver.com
       </p>
     </footer>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,13 @@ import coteLogo from "../assets/cote-logo.png";
 
 function Header() {
   return (
-    <header className="tw-flex tw-h-100 tw-w-full tw-items-center tw-justify-between tw-bg-black tw-px-20">
+    <header className="tw-flex tw-w-full tw-items-center tw-justify-between tw-bg-gray-200 tw-px-20 tw-py-20">
       <a href="/">
         <img src={coteLogo} className="tw-h-50 tw-w-50" alt="cote-cote-logo" />
       </a>
       <button
         type="button"
-        className="tw-rounded tw-border tw-border-white tw-px-10 tw-py-5 tw-text-white"
+        className="tw-rounded tw-border tw-border-black tw-px-10 tw-py-5 tw-text-black tw-transition tw-delay-75 tw-ease-in-out  hover:tw-bg-gray-300"
       >
         Logout
       </button>
